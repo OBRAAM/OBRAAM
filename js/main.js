cd7bdd3088cf6de7e61d49bbbd2943972322e26f
OBRAAM <obraamashraf@gmail.com>
2023-02-10 20:07:58 +0200

    bookMark

--- js/main.js
+++ js/main.js
@@ -4,7 +4,15 @@ var input2 = document.getElementById("input2");
 var nameError2 = document.getElementById("nameError2");
 var nameError3 = document.getElementById("nameError3");
 var row = document.getElementById("row");
-var array = [];
+var shownValue = document.getElementById("shownValue");
+
+var array;
+if (localStorage.getItem("array") == null) {
+    array = [];
+} else {
+    array = JSON.parse(localStorage.getItem("array"));
+    dispalyInput(array)
+}
 
 function addInput() {
 
@@ -12,10 +20,13 @@ function addInput() {
     nameError2.style.display = "none";
     nameError3.style.display = "none";
 
+
+
     for (var i = 0; i < array.length; i++) {
-        if (input1.value == array[i]){
+        if (input1.value == array[i].name) {
             nameError3.style.display = "block";
-            deleteFunc(array[i])
+            input1.value = "alredy-exist";
+            input2.value = "alredy-exist";
         }
     }
 
@@ -26,11 +37,20 @@ function addInput() {
         nameError1.style.display = "block";
     } else if (input2.value == "") {
         nameError2.style.display = "block";
+    } else if (input1.value == "alredy-exist" && input2.value == "alredy-exist") {
+        input1.value = "";
+        input2.value = "";
     } else {
         nameError1.style.display = "none";
         nameError2.style.display = "none";
-        array.push(input1.value);
-        dispalyInput(array)
+        var obj = {
+            name: input1.value,
+            url: input2.value
+        };
+        array.push(obj);
+        localStorage.setItem("array", JSON.stringify(array))
+        dispalyInput(array);
+        clear();
     }
 }
 
@@ -39,8 +59,8 @@ function dispalyInput(array) {
     var container = '';
     for (var i = 0; i < array.length; i++) {
         container += `<div class="col-12 thirdCol">
-        <h2 class="h2sec" id="shownValue">`+ input1.value + `</h2>
-        <a class="btn btn-primary visit a1" href="`+ input2.value + `" target="_blank">visit</a>
+        <h2 class="h2sec" >`+ array[i].name + `</h2>
+        <a class="btn btn-primary visit a1" id="shownValue" href="`+ array[i].url + `" target="_blank">visit</a>
         <button class="btn btn-danger visit b1" onclick="deleteFunc(`+ i + `)">Delete</button>
      </div>`
     }
@@ -50,5 +70,11 @@ function dispalyInput(array) {
 function deleteFunc(x) {
     array.splice(x, 1);
     dispalyInput(array);
+    localStorage.setItem("array", JSON.stringify(array))
+
 }
 
+function clear() {
+    input1.value = "";
+    input2.value = "";
+}
